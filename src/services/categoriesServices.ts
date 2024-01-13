8641a8551116db53c170936f154274c10dd58a92
Ramon Rasheed <ramonrash2@gmail.com>
2024-01-13 12:33:35 +0100

    More

---
+++ src/services/categoriesServices.ts
@@ -0,0 +1,30 @@
+import { TypedSupabaseClient } from "@/lib/types";
+
+export const getAllCategories = (client: TypedSupabaseClient) => {
+  return client.from("catgories").select("*");
+};
+
+export const getCategoryById = (client: TypedSupabaseClient, id: string) => {
+  return client.from("catgories").select("*").eq("id", id).single();
+};
+
+// TODO: Change Type Any
+export const createCategories = (
+  client: TypedSupabaseClient,
+  category: any
+) => {
+  return client.from("categories").insert(category);
+};
+
+export const updateCategory = (client: TypedSupabaseClient, category: any) => {
+  return client
+    .from("categories")
+    .update(category)
+    .eq("id", category.id)
+    .select()
+    .single();
+};
+
+export const deleteCategory = (client: TypedSupabaseClient, id: string) => {
+  return client.from("categories").delete().eq("id", id);
+};
